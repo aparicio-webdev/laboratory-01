e4cf0e2aca83d529ff8b09d6fba38dcf4aaf1660
Anthony John Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2023-10-02 23:12:27 +0800

    Improved code structure

--- src/async.ts
+++ src/async.ts
@@ -1,53 +1,54 @@
 import * as readlineSync from 'readline-sync';
 import * as fs from 'fs/promises';
-
-export const debtsFile: string = 'src/debts.txt';
+import { debtsFile } from './callback';
 
 async function readDebt() {
-        try {
-            const debtList = await fs.readFile(debtsFile, { encoding: 'utf-8', flag: 'r' });
-            console.log(debtList);
-        }
-        catch (err) {
-            console.log("Error occured: ", err)
-        }
+  try {
+    const debtList = await fs.readFile(debtsFile, {
+      encoding: 'utf-8',
+      flag: 'r'
+    });
+    console.log(debtList);
+  } catch (err) {
+    console.log('Error occured: ', err);
+  }
 }
-
 
 async function appendDebt() {
-    try {
-        for (;;) {
-            const input = readlineSync.question("Who are you and what is your debt? ");
+  try {
+    for (;;) {
+      const input = readlineSync.question(
+        'Who are you and what is your debt? '
+      );
 
-            if (input === 'stop') break;
-            if (!input.includes(' ')) {
-                console.log("Invalid format.");
-                throw new Error("Invalid format");
-            }
+      if (input === 'stop') break;
+      if (!input.includes(' ')) {
+        console.log('Invalid format.');
+        throw new Error('Invalid format');
+      }
 
-            const [_name, debtAsString] = input.split(' ');
-            const debt = Number(debtAsString);
+      const [_name, debtAsString] = input.split(' ');
+      const debt = Number(debtAsString);
 
-            if (!isNaN(debt)) {
-                await fs.writeFile(debtsFile, input + '\n', { flag: "a+" });
-            } else {
-                console.log("Not a number!");
-            }
-        }
-    } catch (error) {
-        console.log("An error occurred!", error);
-        throw error;
+      if (!isNaN(debt)) {
+        await fs.writeFile(debtsFile, input + '\n', { flag: 'a+' });
+      } else {
+        console.log('Not a number!');
+      }
     }
+  } catch (error) {
+    console.log('An error occurred!', error);
+    throw error;
+  }
 }
 
-
 async function listDebt() {
-    try {
-        await appendDebt();
-        await readDebt();
-    } catch (error) {
-        console.log("An error occurred: ", error);
-    }
+  try {
+    await appendDebt();
+    await readDebt();
+  } catch (error) {
+    console.log('An error occurred: ', error);
+  }
 }
 
 listDebt();
--- src/promise.ts
+++ src/promise.ts
@@ -1,61 +1,44 @@
 import * as readlineSync from 'readline-sync';
-import * as fs from 'fs';
-
-export const debtsFile: string = 'src/debts.txt';
-
-function readFile(): Promise<string> {
-    return new Promise((resolve, reject) => {
-        fs.readFile(debtsFile, { encoding: 'utf-8', flag: 'r' }, (err, result) => {
-            if (err) {
-                reject(err);
-            }
-            else {
-                resolve(result);
-            }
-        })
-    })
-}
+import * as fs from 'fs/promises';
+import { debtsFile } from './callback';
 
 function appendDebt(): Promise<void> {
-    return new Promise((resolve, reject) => {
-        for (;;) {
-            const input = readlineSync.question("Who are you and what is your debt? ");
+  return new Promise((resolve, reject) => {
+    for (;;) {
+      const input = readlineSync.question(
+        'Who are you and what is your debt? '
+      );
 
-            if (input === 'stop') {
-                resolve();
-                break;
-            }
-            
-            if (!input.includes(' ')) {
-                console.log("Invalid format.");
-                reject();
-                break;
-            }
+      if (input === 'stop') {
+        break;
+      }
 
-            const [_name, debtAsString] = input.split(' ');
-            const debt = Number(debtAsString);
+      if (!input.includes(' ')) {
+        console.log('Invalid format.');
+        reject();
+        break;
+      }
 
-            if (!isNaN(debt)) {
-                    fs.writeFile(debtsFile, input + '\n', {flag: "a+" }, (err) => {
-                        if (err) {
-                            console.log("Error writing file!");
-                            reject(err);
-                        }
-                    })
-            } else {
-                console.log("Not a number!");
-                reject();
-                break;
-            }
-        }
-    });
-}
+      const [_name, debtAsString] = input.split(' ');
+      const debt = Number(debtAsString);
 
-function displayDebt(result: string) {
-    console.log(result);
+      if (!isNaN(debt)) {
+        fs.writeFile(debtsFile, input + '\n', { flag: 'a+' });
+      } else {
+        console.log('Not a number!');
+
+        break;
+      }
+    }
+    resolve();
+  });
 }
 
 appendDebt()
-    .then(readFile)
-    .then(displayDebt)
-    .catch((err) => console.log("An error occured!", err));
+  .then(() => {
+    return fs.readFile(debtsFile, { encoding: 'utf-8', flag: 'r' });
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => console.log('An error occured!', err));
